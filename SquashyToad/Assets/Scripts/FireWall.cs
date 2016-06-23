d5187d23dc04a3eeb49f7604a009d62407d89e53
marc carlyon <irresistiblejelly@gmail.com>
2016-06-23 21:42:54 +0100

    L70 Creating a VR Splash Screen

--- SquashyToad/Assets/Scripts/FireWall.cs
+++ SquashyToad/Assets/Scripts/FireWall.cs
@@ -3,16 +3,30 @@ using System.Collections;
 
 public class FireWall : MonoBehaviour {
 
+    public float creepSpeed = 0.01f;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
+        FollowPlayer();
+        CreepForward();
+
+    }
+
+    void FollowPlayer()
+    {
         GameObject player = GameObject.Find("Player");
         Vector3 delta = player.transform.position - transform.position;
         Vector3 projectedDelta = Vector3.Project(delta, Vector3.left);
         transform.position += projectedDelta;
-	}
+    }
+
+    void CreepForward()
+    {
+        transform.position += Vector3.forward * creepSpeed * Time.deltaTime;
+    }
 }
