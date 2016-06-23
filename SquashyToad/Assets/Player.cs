39705dda17bf0f5845fa3c08efcc7823eb4d42e8
marc carlyon <irresistiblejelly@gmail.com>
2016-06-23 10:53:49 +0100

    L63 Natural Jumping in VR

--- SquashyToad/Assets/Player.cs
+++ SquashyToad/Assets/Player.cs
@@ -2,19 +2,27 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class GazeTest : MonoBehaviour {
+public class Player : MonoBehaviour {
 
     public Text gazeText;
 
     private CardboardHead gazeTestHead;
+    private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
+        Cardboard.SDK.OnTrigger += PullTrigger;
         gazeTestHead = GameObject.FindObjectOfType<CardboardHead>();
+        rb = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void PullTrigger()
+    {
+        rb.AddForce(gazeTestHead.Gaze.direction * 1000);
+    }
+
+    // Update is called once per frame
+    void Update () {
         gazeText.text = gazeTestHead.Gaze.ToString();
 	}
 }
