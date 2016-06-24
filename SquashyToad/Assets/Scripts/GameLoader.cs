7963b404b01d2c6afa83e4eaf0c1af2602572dcd
marc carlyon <irresistiblejelly@gmail.com>
2016-06-24 09:37:20 +0100

    L72 In-Game Menus in VR

---
+++ SquashyToad/Assets/Scripts/GameLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadGame()
+    {
+        SceneManager.LoadScene("Main");
+        Debug.Log("Load Request");
+    }
+}
