cba7fe77a1bd9fd2b199f5064871c8c3697e3f96
marc carlyon <irresistiblejelly@gmail.com>
2016-06-24 13:13:21 +0100

    L78 Making the vehicles move

---
+++ SquashyToad/Assets/Scripts/VehicleSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleSpawner : MonoBehaviour {
+
+    public GameObject[] vehiclePrefabs;
+    public float heightOffset = 1f;
+    public float startOffset = -10f;
+
+    // Use this for initialization
+   
+    void Start () {
+        Vector3 position = transform.position;
+        position += heightOffset * Vector3.up;
+        position += startOffset * Vector3.right;
+        GameObject vehicleObject = Instantiate(vehiclePrefabs[0]);
+        vehicleObject.transform.position = position;
+        vehicleObject.transform.parent = transform;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
